be31db25440b49984100c5ba94a1ca8141edda4f
a110605 <a1106052000@gmail.com>
2017-08-28 18:01:40 +0800

    first commit

--- github-init-project.sh
+++ github-init-project.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-read -p "Choose the options to use:\n
-	[1] Init a project on github.\n
-	[2] Push files to github.\n" options
-echo "$options"	 
-echo "# $1" >> README.md
-git init
-git add .
-git commit -m "first commit"
-git remote add origin https://github.com/a110605/$1.git
-git push -u origin master
+read -p "Choose the options to use:
+	[1] Init a project on github.
+	[2] Push files to github.
+	" options
+
+case $options in
+    1 ) read -p "What is repository name: " name
+	echo "# ${name}" >> README.md
+	git init
+	git add .
+	git commit -m "first commit"
+	git remote add origin https://github.com/a110605/$name.git
+	git push -u origin master
+	break;;
+    * ) echo "Please choose options 1 or 2";
+        exit;
+    	break ;;
+esac
